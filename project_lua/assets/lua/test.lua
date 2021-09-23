6ec9fb2426193918c7559b0dbd366fa438674ca9
vika <757058941@qq.com>
2021-09-23 10:30:50 +0800

    update

--- project_lua/assets/lua/test.lua
+++ project_lua/assets/lua/test.lua
@@ -1,9 +1,31 @@
 function app()
-	return Text:new("Flutter", {
-		style = {
-			fontSize = sp(100),
-            fontWeight = FontWeight.w200
+	return Row:new({
+        MainAxisAlignment.center
+		children = {
+			Text:new("Flutter", {
+				style = {
+					fontSize = sp(100),
+					fontWeight = FontWeight.w200
+				}
+			}),
+			Text:new("Flutter1", {
+				style = {
+					fontSize = sp(100),
+					fontWeight = FontWeight.w200
+				}
+			}),
+			Text:new("Flutter2", {
+				style = {
+					fontSize = sp(200),
+					fontWeight = FontWeight.w800
+				}
+			})
 		}
 	});
 end;
+
+
+
+
+
 print("hello world");
